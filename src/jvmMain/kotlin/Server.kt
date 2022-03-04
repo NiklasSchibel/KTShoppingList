082c20dbc48870a4857881e36b0536f99fc272e0
NiklasSchibel <95278041+NiklasSchibel@users.noreply.github.com>
2022-03-04 21:17:10 +0100

    setup Tutorial start

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,42 @@
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.*
+import org.litote.kmongo.reactivestreams.KMongo
+import com.mongodb.ConnectionString
+import io.netty.handler.codec.compression.StandardCompressionOptions
+import io.netty.handler.codec.compression.StandardCompressionOptions.gzip
+import org.graalvm.compiler.hotspot.nodes.type.MethodPointerStamp
+import org.graalvm.compiler.hotspot.nodes.type.MethodPointerStamp.method
+
+
 fun main() {
-    println("Hello, JVM!")
+
+    embeddedServer(Netty, 9090) {
+        install(ContentNegotiation) {
+            json()
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            anyHost()
+        }
+        install(Compression) {
+            StandardCompressionOptions.gzip()
+        }
+        routing {
+            get("/hello") {
+                call.respondText("Hello, API!")
+            }
+        }
+    }.start(wait = true)
 }
